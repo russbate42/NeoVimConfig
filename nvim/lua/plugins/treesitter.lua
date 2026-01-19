49c2975282039b4cdbb7482266d78899e4cf9be3
Russell Bate <r.bate@hotmail.com>
2026-01-19 05:43:24 +0100

    Fixed tree-sitter zsh language bug. Fixed diagnostic signs error with LSP.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,26 @@
 -- print('In treesitter')
 return {
-    {"nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     branch = 'master',
     lazy = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "lua", "vim", "markdown", "bash", "python", "javascript",
-            "typescript", "html", "css", "json", "yaml", "bibtex", "zsh"
-        },
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "latex" },
-        },
-        indent = {
-            enable = true,
-        },
-        autotag = {
-            enable = true,
-        }
+            ensure_installed = {
+                "lua", "vim", "markdown", "bash", "python", "javascript",
+                "typescript", "html", "css", "json", "yaml", "bibtex"
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "latex" },
+            },
+            indent = {
+                enable = true,
+            },
+            autotag = {
+                enable = true,
+            }
         })
     end
-    }
 }
