be83eecb55db24152cec5d5e730aae0f0ba5f043
Russell Bate <r.bate@hotmail.com>
2026-09-20 20:00:52 +0200

    Added input hints hotkey to functions in lsp.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,6 +52,9 @@ return {
                 opts.desc = "Show documentation for what is under cursor"
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
+                opts.desc = "Show argument hints inside function call"
+                vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+
                 opts.desc = "Restart LSP"
                 vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
             end,
